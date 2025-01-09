d938ccd029ee71373519e17cd1037cb995532c78
AbdulrahmanFa2hy <abdelrhman.fa2hy@gmail.com>
2025-01-09 11:09:36 +0200

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-right': 'bounce-right 1s infinite', // Custom animation
+      },
+      keyframes: {
+        'bounce-right': {
+          '0%, 100%': { transform: 'translateX(0)' }, // Start and end at the original position
+          '50%': { transform: 'translateX(25px)' }, // Move 25px to the right
+        },
+      },
+    },
   },
   plugins: [],
 }
